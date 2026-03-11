b214dd18b2152572442c34c4e8a5c72c231dafce
moshe1ch-kidi <moshe1.ch@gmail.com>
2026-03-11 18:38:11 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import tailwindcss from '@tailwindcss/vite';
+ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
@@ -7,8 +7,8 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   
   return {
-    // החלף את 'your-repo-name' בשם המדויק של ה-Repository שלך ב-GitHub
-    base: '/your-repo-name/', 
+    // הגדרת הנתיב עבור GitHub Pages
+    base: '/pngtosvg/', 
     
     plugins: [react(), tailwindcss()],
     define: {
